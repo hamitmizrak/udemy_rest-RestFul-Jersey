ddf1bd87473d6a5cb8a46003ac4d457aff4363ff
HamitMizrak <hamitmizrak@gmail.com>
2021-03-31 15:36:32 +0300

    Produces text/plain; UTF-8

---
+++ src/com/hamitmizrak/common/CoomonInformation.java
@@ -0,0 +1,12 @@
+package com.hamitmizrak.common;
+
+public class CoomonInformation {
+
+	/*
+	 * URL(Uniform Resource Locator) ==> sabit ip  www.hamitmizrak.com  site adı
+	 * URI(Uniform Resource Identifier): ==> www.hamitmizrak.com/javase-dersleri
+	 * (tam adres) CSV : ==> Virgüllerle ayrılmış düz metin dosyaları gösteren veri
+	 * listesine denir. Escape character ==> \
+	 * 
+	 */
+}
---
+++ src/com/hamitmizrak/webjersey/RestProduces.java
@@ -0,0 +1,19 @@
+package com.hamitmizrak.webjersey;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+// localhos:8080/JerseyRest/rest/Path
+
+@Path("/produces")
+public class RestProduces {
+
+	@GET
+	// @Produces(MediaType.TEXT_PLAIN)
+	@Produces("text/plain; charset=UTF-8")
+	public String methodProduces() {
+		return "Hamit Mızrak üğşçöÜĞŞÇÖ";
+	}
+
+}
